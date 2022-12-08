70cf0f1f7e36da118e14bcc598f9f968488c497e
kamel <kamdemd@gmail.com>
2022-12-08 09:36:43 +0100

    3update

---
+++ src/main/java/com/rootar/hibernate_rootar/entities/MonnaieEntity.java
@@ -0,0 +1,52 @@
+package com.rootar.hibernate_rootar.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "MONNAIE", schema = "dbo", catalog = "ROOTAR")
+public class MonnaieEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "ID_MONNAIE")
+    private int idMonnaie;
+    @Basic
+    @Column(name = "LIBELLE_MONNAIE")
+    private String libelleMonnaie;
+
+    public int getIdMonnaie() {
+        return idMonnaie;
+    }
+
+    public void setIdMonnaie(int idMonnaie) {
+        this.idMonnaie = idMonnaie;
+    }
+
+    public String getLibelleMonnaie() {
+        return libelleMonnaie;
+    }
+
+    public void setLibelleMonnaie(String libelleMonnaie) {
+        this.libelleMonnaie = libelleMonnaie;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MonnaieEntity that = (MonnaieEntity) o;
+
+        if (idMonnaie != that.idMonnaie) return false;
+        if (libelleMonnaie != null ? !libelleMonnaie.equals(that.libelleMonnaie) : that.libelleMonnaie != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idMonnaie;
+        result = 31 * result + (libelleMonnaie != null ? libelleMonnaie.hashCode() : 0);
+        return result;
+    }
+}
