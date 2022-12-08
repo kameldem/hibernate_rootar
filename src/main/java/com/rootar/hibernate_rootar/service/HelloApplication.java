746bd7bd98126f7c8f2b2824fa4a3ae26b83ef37
kamel <kamdemd@gmail.com>
2022-12-08 09:30:13 +0100

    2update

---
+++ src/main/java/com/rootar/hibernate_rootar/service/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.rootar.hibernate_rootar.service;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
